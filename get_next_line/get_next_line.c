48908eaa60283da9980f985476de2309bf1e88ac
Mehdi Belkassi <mehdibelkassi1@gmail.com>
2024-11-29 11:56:40 +0100

    Add files via upload

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,145 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbelkass <mbelkass@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 04:50:26 by mbelkass          #+#    #+#             */
+/*   Updated: 2024/11/29 10:54:16 by mbelkass         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+char	*ft_free(char *res, char *buffer)
+{
+	char	*temp;
+
+	temp = ft_strjoin(res, buffer);
+	free(res);
+	return (temp);
+}
+
+char	*ft_next(char *buffer)
+{
+	int		i;
+	int		j;
+	char	*line;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	line = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
+	i++;
+	j = 0;
+	while (buffer[i])
+		line[j++] = buffer[i++];
+	free(buffer);
+	return (line);
+}
+
+char	*read_file(int fd, char *res)
+{
+	char	*buffer;
+	int		byte_read;
+
+	if (!res)
+		res = ft_calloc(1, 1);
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	byte_read = 1;
+	while (byte_read > 0)
+	{
+		byte_read = read(fd, buffer, BUFFER_SIZE);
+		if (byte_read == -1)
+		{
+			free(buffer);
+			free(res);
+			res = NULL;
+			return (NULL);
+		}
+		buffer[byte_read] = 0;
+		res = ft_free(res, buffer);
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	free(buffer);
+	return (res);
+}
+
+char	*ft_line(char *buffer)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	line = ft_calloc(i + 2, sizeof(char));
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] && buffer[i] == '\n')
+		line[i++] = '\n';
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	{
+		if (buffer)
+		{
+			free(buffer);
+			buffer = NULL;
+		}
+		return (NULL);
+	}
+	buffer = read_file(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	line = ft_line(buffer);
+	buffer = ft_next(buffer);
+	return (line);
+}
+
+int main()
+{
+	char *name = "file.txt";
+		int fd = open(name, O_RDONLY);
+		/* 1 */ printf("%s",get_next_line(fd));
+		/* 2 */ printf("%s",get_next_line(fd));
+		// set the next read call to return -1
+		// if (BUFFER_SIZE > 100) {
+		// 	char *temp;
+		// 	do {
+		// 		temp = get_next_line(fd);
+		// 		free(temp);
+		// 	} while (temp != NULL);
+		// }
+		/* 3 */ printf("%s",get_next_line(fd));
+		close(fd);
+		fd = open(name, O_RDONLY);
+		/* 4 */ printf("%s",get_next_line(fd));
+		/* 5 */ printf("%s",get_next_line(fd));
+		/* 6 */ printf("%s",get_next_line(fd));
+		/* 7 */ printf("%s",get_next_line(fd));
+		/* 8 */ printf("%s",get_next_line(fd));
+}
